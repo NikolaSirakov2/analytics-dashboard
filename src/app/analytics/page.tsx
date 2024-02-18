245322c66f95de0af1205455d93931719fa4ab28
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-02-18 10:31:26 +0200

    Create page.tsx

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+        <h1>Analytics</h1>
+        <p style={{ color: 'white' }}>Analytics page content</p>
+        </div>
+    );
+    };
+    export default Page;
