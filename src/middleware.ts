ce7f47f73f335d18dd8fcca7f3d1fba5cf0833bc
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-02-18 10:39:32 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,21 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
     if(req.nextUrl.pathname === "/") {
         console.error("Redirecting to /home");
+        try{
+            analythics.track("pageview", {
+                page: "/",
+                countrry: req.geo?.country,
+            });
+        } catch (e) {
+            console.error(e);
+        }
     }
+
+    return NextResponse.next();
+}
+
+export const matcher = {
+    matcher: ['/']
 }
