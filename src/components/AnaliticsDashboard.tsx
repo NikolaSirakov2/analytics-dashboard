da90995761e29fe3c554255627a8ed0662256895
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-02-21 14:01:20 +0200

    Update AnaliticsDashboard.tsx

--- src/components/AnaliticsDashboard.tsx
+++ src/components/AnaliticsDashboard.tsx
@@ -2,7 +2,11 @@
 
 import { Card } from "@tremor/react";
 
-const AnalyticsDashboard = () => {
+interface AnalyticsDashboardProps {
+    avgPageViews: string;
+}
+
+const AnalyticsDashboard = ( { avgPageViews } : AnalyticsDashboardProps) => {
   return (
     <div className="flex flex-col gap-6">
       <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
@@ -10,6 +14,9 @@ const AnalyticsDashboard = () => {
           <p className="text-tremor-default text-dark-tremor-content">
             Pageviews/day
           </p>
+          <p className="text-3xl text-dark-tremor-content font-semibold">
+            {avgPageViews}
+             </p>
         </Card>
       </div>
     </div>
