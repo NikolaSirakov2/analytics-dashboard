e2a209b8a7eb9564e7b01f23a03524e0ddbf1bf4
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-02-21 13:55:42 +0200

    Update AnaliticsDashboard.tsx

--- src/components/AnaliticsDashboard.tsx
+++ src/components/AnaliticsDashboard.tsx
@@ -1,11 +1,19 @@
+"use client";
+
+import { Card } from "@tremor/react";
+
 const AnalyticsDashboard = () => {
-    return (
-        <div className="flex flex-col gap-6">
-            <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
-                
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Pageviews/day
+          </p>
+        </Card>
+      </div>
+    </div>
+  );
+};
 
 export default AnalyticsDashboard;
