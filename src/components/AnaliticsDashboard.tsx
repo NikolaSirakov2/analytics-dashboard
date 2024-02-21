7ffd7b1f5ffdfc4cc4b1e64eee38cc71951c5666
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-02-21 14:52:58 +0200

    Update AnaliticsDashboard.tsx

--- src/components/AnaliticsDashboard.tsx
+++ src/components/AnaliticsDashboard.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { Card } from "@tremor/react";
+import { analytics } from "@/utils/analytics";
+import { BarChart, Card } from "@tremor/react";
 
 interface AnalyticsDashboardProps {
     avgPageViews: string;
     amtVisitorsToday: number;
+    timeseriesPageViews: Awaited<ReturnType<typeof analytics.retriveDays>>;
 }
 
-const AnalyticsDashboard = ( { avgPageViews, amtVisitorsToday } : AnalyticsDashboardProps) => {
+const AnalyticsDashboard = ( { avgPageViews, amtVisitorsToday, timeseriesPageViews } : AnalyticsDashboardProps) => {
   return (
     <div className="flex flex-col gap-6">
       <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
@@ -28,6 +30,23 @@ const AnalyticsDashboard = ( { avgPageViews, amtVisitorsToday } : AnalyticsDashb
           </p>
         </Card>
       </div>
+
+      <Card>
+        {timeseriesPageViews ? (
+            <BarChart
+                allowDecimals={false}
+                showAnimation
+                data={timeseriesPageViews.map((day) => ({
+                    name: day.date,
+                    Visitors: day.events.reduce((acc, cur) => {
+                        return acc + Object.values(cur)[0]!;
+                    }, 0),
+                }))}
+                categories={['Visitors']}
+                index='name'
+            />
+            ) : null}
+        </Card>
     </div>
   );
 };
