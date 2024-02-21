c669bc958b695dd7acc2bdcca37671a36bc73019
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-02-21 14:01:17 +0200

    Update page.tsx

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -2,13 +2,14 @@ import AnalyticsDashboard from "@/components/AnaliticsDashboard";
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
+    const TRACKING_DAYS = 7;
 
     const pageview = await analytics.retriveDays("pageview", 2);
 
     return (
         <div className="min-h-screen w-full py-12 flex justify-center items-center">
             <div className="relative w-full max-w-6xl mx-auto text-white">
-                <AnalyticsDashboard />
+                <AnalyticsDashboard avgPageViews=""/>
             </div>
         </div>
     );
